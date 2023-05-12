899347ab0121e0bff48859338fed4d715521aae7
gavsidhu <gavinjeet.sidhu@gmail.com>
2023-05-12 12:30:39 -0700

    changed prod url

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,26 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
 export const config = {
-  matcher: ["/((?!api/|_next/|_static/|examples/|[\\w-]+\\.\\w+).*)"],
+  matcher: ['/((?!api/|_next/|_static/|examples/|[\\w-]+\\.\\w+).*)'],
 };
 
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
-  const hostname = req.headers.get("host") || "quick-val.vercel.app";
+  const hostname = req.headers.get('host') || 'quickval.co';
   const path = url.pathname;
 
   const currentHost =
-    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname.replace(`.quick-val.vercel.app`, "")
-      : hostname.replace(`.localhost:3000`, "");
+    process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
+      ? hostname.replace(`.quickval.co`, '')
+      : hostname.replace(`.localhost:3000`, '');
 
-  if (currentHost == "app") {
+  if (currentHost == 'app') {
     url.pathname = `/app${url.pathname}`;
     return NextResponse.rewrite(url);
   }
 
   // rewrite root application to `/home` folder
-  if (hostname === "quick-val.vercel.app" || hostname === "localhost:3000") {
+  if (hostname === 'quickval.co' || hostname === 'localhost:3000') {
     return NextResponse.rewrite(new URL(`/home${path}`, req.url));
   }
 
